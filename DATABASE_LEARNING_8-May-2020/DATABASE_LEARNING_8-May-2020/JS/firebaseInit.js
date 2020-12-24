0b0703f35f9373f9c1ce9e959b1fe64d2ded8c2d
daniya-pro <syedadaniyahaider@gmail>
2020-12-24 22:11:55 +0500

    fixed some bugs

---
+++ DATABASE_LEARNING_8-May-2020/DATABASE_LEARNING_8-May-2020/JS/firebaseInit.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD0EWi5ZSFuNQl5pFQ2dmOLDdrBERkoDrc",
+    authDomain: "my-first-project-bc7a3.firebaseapp.com",
+    databaseURL: "https://my-first-project-bc7a3.firebaseio.com",
+    projectId: "my-first-project-bc7a3",
+    storageBucket: "my-first-project-bc7a3.appspot.com",
+    messagingSenderId: "503270154462",
+    appId: "1:503270154462:web:71f0095d8d727bbb4ea24f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.database
